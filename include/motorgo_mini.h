2ac924c573b773c8e3c357c2e97d7670a46f0795
Swapnil Pande <swapnil.pande98@gmail.com>
2024-02-08 16:45:01 -0500

    Switch to using new pin def names

--- include/motorgo_mini.h
+++ include/motorgo_mini.h
@@ -13,27 +13,27 @@ class MotorGoMini
 {
  private:
   BLDCChannelParameters ch0_params = {
-      .uh = CH0_GPIO_UH,
-      .ul = CH0_GPIO_UL,
-      .vh = CH0_GPIO_VH,
-      .vl = CH0_GPIO_VL,
-      .wh = CH0_GPIO_WH,
-      .wl = CH0_GPIO_WL,
+      .uh = CH0_UH,
+      .ul = CH0_UL,
+      .vh = CH0_VH,
+      .vl = CH0_VL,
+      .wh = CH0_WH,
+      .wl = CH0_WL,
       .current_u = CH0_CURRENT_U,
-      .current_v = GPIO_NOT_SET,
+      .current_v = CH0_CURRENT_V,
       .current_w = CH0_CURRENT_W,
       .enc_cs = CH0_ENC_CS,
   };
 
   BLDCChannelParameters ch1_params = {
-      .uh = CH1_GPIO_UH,
-      .ul = CH1_GPIO_UL,
-      .vh = CH1_GPIO_VH,
-      .vl = CH1_GPIO_VL,
-      .wh = CH1_GPIO_WH,
-      .wl = CH1_GPIO_WL,
+      .uh = CH1_UH,
+      .ul = CH1_UL,
+      .vh = CH1_VH,
+      .vl = CH1_VL,
+      .wh = CH1_WH,
+      .wl = CH1_WL,
       .current_u = CH1_CURRENT_U,
-      .current_v = GPIO_NOT_SET,
+      .current_v = CH1_CURRENT_V,
       .current_w = CH1_CURRENT_W,
       .enc_cs = CH1_ENC_CS,
   };
