26aa6457907430e2e129eadf7b5c63d4c5dd33bd
Swapnil Pande <swapnil.pande98@gmail.com>
2024-02-08 14:48:52 -0500

    Variable name changes in two motors

--- examples/spin_two_motors.cpp
+++ examples/spin_two_motors.cpp
@@ -6,11 +6,11 @@ MotorGo::MotorGoMini motorgo_mini;
 MotorGo::MotorChannel& motor_left = motorgo_mini.ch0;
 MotorGo::MotorChannel& motor_right = motorgo_mini.ch1;
 
-MotorGo::MotorParameters motor_params_ch0;
-MotorGo::MotorParameters motor_params_ch1;
+MotorGo::MotorParameters motor_params_left;
+MotorGo::MotorParameters motor_params_right;
 
-MotorGo::PIDParameters velocity_pid_params_ch0;
-MotorGo::PIDParameters velocity_pid_params_ch1;
+MotorGo::PIDParameters velocity_pid_params_left;
+MotorGo::PIDParameters velocity_pid_params_right;
 
 // Function to print at a maximum frequency
 void freq_println(String str, int freq)
@@ -32,43 +32,43 @@ void setup()
   delay(3000);
 
   // Setup motor parameters
-  motor_params_ch0.pole_pairs = 7;
-  motor_params_ch0.power_supply_voltage = 5.0;
-  motor_params_ch0.voltage_limit = 5.0;
-  motor_params_ch0.current_limit = 300;
-  motor_params_ch0.velocity_limit = 100.0;
-  motor_params_ch0.calibration_voltage = 2.0;
-  motor_params_ch0.reversed = false;
+  motor_params_left.pole_pairs = 7;
+  motor_params_left.power_supply_voltage = 5.0;
+  motor_params_left.voltage_limit = 5.0;
+  motor_params_left.current_limit = 300;
+  motor_params_left.velocity_limit = 100.0;
+  motor_params_left.calibration_voltage = 2.0;
+  motor_params_left.reversed = false;
 
-  motor_params_ch1.pole_pairs = 7;
-  motor_params_ch1.power_supply_voltage = 5.0;
-  motor_params_ch1.voltage_limit = 5.0;
-  motor_params_ch1.current_limit = 300;
-  motor_params_ch1.velocity_limit = 100.0;
-  motor_params_ch1.calibration_voltage = 2.0;
-  motor_params_ch1.reversed = true;
+  motor_params_right.pole_pairs = 7;
+  motor_params_right.power_supply_voltage = 5.0;
+  motor_params_right.voltage_limit = 5.0;
+  motor_params_right.current_limit = 300;
+  motor_params_right.velocity_limit = 100.0;
+  motor_params_right.calibration_voltage = 2.0;
+  motor_params_right.reversed = true;
 
   // Setup Ch0
   bool calibrate = false;
-  motor_left.init(motor_params_ch0, calibrate);
-  motor_right.init(motor_params_ch1, calibrate);
+  motor_left.init(motor_params_left, calibrate);
+  motor_right.init(motor_params_right, calibrate);
 
   // Set velocity controller parameters
   // Setup PID parameters
-  velocity_pid_params_ch0.p = 1.6;
-  velocity_pid_params_ch0.i = 0.01;
-  velocity_pid_params_ch0.d = 0.0;
-  velocity_pid_params_ch0.output_ramp = 10000.0;
-  velocity_pid_params_ch0.lpf_time_constant = 0.11;
+  velocity_pid_params_left.p = 1.6;
+  velocity_pid_params_left.i = 0.01;
+  velocity_pid_params_left.d = 0.0;
+  velocity_pid_params_left.output_ramp = 10000.0;
+  velocity_pid_params_left.lpf_time_constant = 0.11;
 
-  velocity_pid_params_ch1.p = 1.6;
-  velocity_pid_params_ch1.i = 0.01;
-  velocity_pid_params_ch1.d = 0.0;
-  velocity_pid_params_ch1.output_ramp = 10000.0;
-  velocity_pid_params_ch1.lpf_time_constant = 0.11;
+  velocity_pid_params_right.p = 1.6;
+  velocity_pid_params_right.i = 0.01;
+  velocity_pid_params_right.d = 0.0;
+  velocity_pid_params_right.output_ramp = 10000.0;
+  velocity_pid_params_right.lpf_time_constant = 0.11;
 
-  motor_left.set_velocity_controller(velocity_pid_params_ch0);
-  motor_right.set_velocity_controller(velocity_pid_params_ch1);
+  motor_left.set_velocity_controller(velocity_pid_params_left);
+  motor_right.set_velocity_controller(velocity_pid_params_right);
 
   //   Set closed-loop velocity mode
   motor_left.set_control_mode(MotorGo::ControlMode::Velocity);
